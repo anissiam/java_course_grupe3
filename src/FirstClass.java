e3d4bc143fcab68d40a7486966c749fa8c979e75
anis <anis.siam@hotmail.com>
2022-10-07 08:41:48 +0300

    first commit

---
+++ src/FirstClass.java
@@ -0,0 +1,18 @@
+public class FirstClass {
+    int x;// Global
+    boolean y = false;
+    public static void main(String[] args) {
+        //int x = 10; // LOCAL
+        //boolean zz = true;
+
+
+
+
+        //this is print anis
+        System.out.println("Anis "); //this is commnet
+        System.out.println("siam"); /* this is mutile line of comments
+         */
+
+
+    }
+}
---
+++ src/Var.java
@@ -0,0 +1,25 @@
+public class Var {
+    public static void main(String[] args) {
+
+        int x= 10;
+        //int x = 100;
+        System.out.println(x);
+        x = 100;
+        /*byte y = 100;
+        y = 50;*/
+        System.out.println(x);
+
+        int xx;
+        xx = 90;
+        System.out.println(xx);
+        System.out.println("This is xx["+xx+"]" );
+
+        int v = 10;
+        v = xx;
+        System.out.println(v + " " + xx);
+
+        System.out.println("50");
+        x = 50;
+        System.out.println(x);
+    }
+}
